fcb0ebc27d34d6bf023f8ad896c0dabd584a6c76
bwinckell <bwinckell@companeo.com>
2017-09-11 10:39:29 +0200

    add config file

---
+++ config.js
@@ -0,0 +1,11 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  trigger_hours: ['12:00','10:26']
+}
+
